530e3ea2cc6c7ff097448aadb9ce1e10db0a35ca
Hexagon Six <hexagon6@fet.li>
2022-06-01 16:49:56 +0200

    deployment: add sveltejs-adapter-ipfs for ipfs deployment, currently not working

--- viewer/svelte.config.js
+++ viewer/svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess'
+import adapter_ipfs from 'sveltejs-adapter-ipfs'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter_ipfs({
+			removeBuiltInServiceWorkerRegistration: true,
+			injectPagesInServiceWorker: true,
+		}),
+	},
+}
 
-export default config;
+export default config
