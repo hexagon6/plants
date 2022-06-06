c9abf48fa9c5250022630829ecc23d90ec905f3b
Hexagon Six <hexagon6@fet.li>
2022-06-06 16:50:14 +0200

    feature(viewer): implement svgTurtle

--- viewer/src/lib/turtle.js
+++ viewer/src/lib/turtle.js
@@ -1,4 +1,4 @@
-import { map, pipe as _, tap } from 'ramda'
+import { concat, filter, map, pipe as _, reduce, tap } from 'ramda'
 
 // const log = tap(console.log)
 export const canvasTurtle =
@@ -61,6 +61,57 @@ export const canvasTurtle =
 				f => f(),
 			),
 		)(input.split(''))
+	}
+
+export const svgTurtle =
+	({ /** @type {number} */ X, /** @type {number} */ Y }) =>
+	({ /** @type {float} */ δ, /** @type {float} */ d, /** @type {boolean} */ doClear }) =>
+	(/** @type {string} */ input) => {
+		const center = {
+			X: X / 2,
+			Y: Y / 2,
+		}
+		let direction = 0 /* 0 = North, 90 = East, 180 = South, 270 = West */
+		let xPos = center.X
+		let yPos = center.Y
+		const actions = {
+			F: () => {
+				// console.log(`move forward 1 step with length d ${d} while drawing a line segment`)
+				const V = (direction / 360) * 2 * Math.PI
+				const x = d * Math.sin(V)
+				const y = -d * Math.cos(V)
+				return `l ${x} ${y}`
+			},
+			f: () => {
+				const V = (direction / 180) * Math.PI
+				const x = d * Math.sin(V)
+				const y = -d * Math.cos(V)
+				console.log(`move forward 1 step with length d ${d} without drawing`)
+				return `m ${x} ${y}`
+			},
+			'-': () => {
+				direction = (direction + δ) % 360
+				console.log(`turn right by angle δ ${δ}, ${direction}`)
+				return ''
+			},
+			'+': () => {
+				direction = (direction + 360 - δ) % 360
+				console.log(`turn left by angle δ ${δ}, ${direction}`)
+				return ''
+			},
+		}
+
+		const path = _(
+			map(
+				_(
+					(/** @type {string} */ char) => (char in actions ? actions[char] : () => {}),
+					f => f(),
+				),
+			),
+			filter(f => f !== ''),
+		)(input.split('')).join(' ')
+		return `M ${center.X},${center.Y}
+		${path}`
 	}
 
 export const allowedStrings = 'Ff-+'.split('')
